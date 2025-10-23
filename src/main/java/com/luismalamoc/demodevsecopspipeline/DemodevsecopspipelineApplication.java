704900161b02082191fc63e77cef59f86ca35c4b
Luis Alamo <luismalamoc@gmail.com>
2025-10-23 19:53:54 -0300

    feat: first commit

---
+++ src/main/java/com/luismalamoc/demodevsecopspipeline/DemodevsecopspipelineApplication.java
@@ -0,0 +1,13 @@
+package com.luismalamoc.demodevsecopspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodevsecopspipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodevsecopspipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luismalamoc/demodevsecopspipeline/DemodevsecopspipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luismalamoc.demodevsecopspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodevsecopspipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
